56c68a0b5b025d5316ccbfbbfa0f1754cbc22d72
Xhuljo Toshkallari <123358950+toshkallari@users.noreply.github.com>
2023-03-01 13:30:17 +0100

    Add files via upload

---
+++ ajuga.py
@@ -0,0 +1,9 @@
+print('ciao mallardi')
+lista=[]
+a=int(input('quanti numeri devi inserire? '))
+for i in range (0,a):
+    n=int(input('inserisci numeri'))
+    lista.append(n)
+    print('stampo una lista')
+print(lista)                                                                                 
+          
